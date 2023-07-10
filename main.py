674819e733e3bef3ff6a8173b531abda2c4e04d7
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2023-07-10 19:56:04 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, send_file
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import Headers
+import tempfile
+import io
+import pgn_utils
+
+app = Flask(__name__)
+
+def send_as_file(filename, content):
+    bytes_io = io.BytesIO(content.encode())
+    bytes_io.seek(0)
+    headers = Headers()
+    headers.add('Content-Disposition', 'attachment', filename='updated_' + filename)
+    return send_file(bytes_io, mimetype='text/plain', as_attachment=True, download_name="custom_name.txt")
+
+@app.route('/upload_pgn', methods=['POST'])
+def take_single_game_from_pgn_return_headers_moves():
+    if 'pgnFile' not in request.files:
+        return "No pgnFile key in request.files"
+
+    file = request.files['pgnFile']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if file:
+        filename = secure_filename(file.filename)
+
+        # Save the file to a temporary location
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        file.save(temp_file.name)
+        temp_file.close()
+
+        # Preprocess the file to remove comments in braces
+        with open(temp_file.name, 'r') as pgn_file:
+            content = pgn_file.read()
+        content = pgn_utils.sanitize_pgn_content(content)
+
+        # Save the preprocessed pgn to a temporary file
+        with open(temp_file.name, 'w') as pgn_file:
+            pgn_file.write(content)
+
+        pgn_content = pgn_utils.execute_pgn_extract_command(temp_file.name)
+        updated_pgn_content = pgn_utils.add_opening_strategy_headers(pgn_content)
+
+        print(updated_pgn_content)
+
+        return send_as_file(filename, updated_pgn_content)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ pgn_utils.py
@@ -0,0 +1,55 @@
+import re
+import os
+import subprocess
+
+def execute_pgn_extract_command(file_path, pgn_extract_path='pgn-extract.exe'):
+    # The following line is commented as you can add the path to pgn-extract.exe to the system PATH
+    # os.chdir('C:/Users/USER/PycharmProjects/pythonProject')  # Change the working directory
+    command = [pgn_extract_path, '-e', 'eco.pgn', file_path]
+    result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
+    return result.stdout
+
+def get_game_headers(game_lines):
+    headers = {}
+    for line in game_lines:
+        if '[ECO ' in line or '[Opening ' in line or '[Variation ' in line or '[White ' in line or '[Black ' in line or '[Result ' in line:
+            key, value = re.search(r'\[(\w+) "(.+)"\]', line).groups()
+            headers[key] = value
+    return headers
+
+def get_book_moves(headers):
+    with open('eco.pgn', 'r') as eco_file:
+        eco_lines = eco_file.readlines()
+        for j, eco_line in enumerate(eco_lines):
+            if f'[ECO "{headers.get("ECO")}"]' in eco_line and f'[Opening "{headers.get("Opening")}"]' in eco_lines[j+1]:
+                book_moves_line = eco_lines[j+4]
+                headers['BookMoves'] = " ".join(book_moves_line.rstrip("*\n").split())
+                break
+    return headers
+
+def sanitize_pgn_content(content):
+    return re.sub(r'{(.*?)}', r'\1', content)  # Remove only the braces
+
+def add_opening_strategy_headers(pgn_content):
+    lines = pgn_content.split('\n')
+    new_lines = []
+
+    game_starts = [i for i, line in enumerate(lines) if '[Event ' in line]
+    for i in range(len(game_starts)):
+        game_lines = lines[game_starts[i]:game_starts[i + 1] if i + 1 < len(game_starts) else len(lines)]
+        headers = get_game_headers(game_lines)
+        if headers.get('White') != '?' and headers.get('Black') != '?':
+            headers = get_book_moves(headers)
+
+        if 'BookMoves' in headers:
+            book_moves_header = f'[BookMoves "{headers["BookMoves"]}"]\n'
+            # find the position of the last header in the game_lines and insert 'BookMoves' after that
+            last_header_index = next((i for i, line in reversed(list(enumerate(game_lines))) if '[' in line), None)
+            if last_header_index is not None:
+                game_lines.insert(last_header_index + 1, book_moves_header)
+                # remove trailing empty line from headers
+                if game_lines[last_header_index + 2] == '':
+                    game_lines.pop(last_header_index + 2)
+                new_lines.extend(game_lines)
+
+    return '\n'.join(new_lines)
